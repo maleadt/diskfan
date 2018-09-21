a02d03da5bba7eb1093e5d7dc3fac052b518f409
Tim Besard <tim.besard@gmail.com>
2018-09-21 12:53:39 +0200

    Tune min duty factor.

--- config.jl
+++ config.jl
@@ -5,6 +5,6 @@ const PERIPHERAL_DISKS = [ "ata-WDC_WD30EFRX-68AX9N0_WD-WMC1T0876678",
 const SYSTEM_DISKS     = [ "ata-WDC_WD30EFRX-68EUZN0_WD-WMC4N2180527" ]
 
 # minimum duty cycle for fans (eg. to prevent them from stopping, and/or maintain airflow)
-const MIN_DUTY         = 2
+const MIN_DUTY         = 5
 # NOTE: disregarding the sensor lower limits, a duty cycle of 2 seems like the minimal value
 #       where the SuperMicro BMC doesn't kick in and makes the fan spin at full power
